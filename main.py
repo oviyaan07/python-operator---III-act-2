ff82f4428a6f3bd5539517da7abc268e8e2becfa
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-10 12:24:07 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+a=[2,4,6,8,10]
+if 5 not in a:
+    print("It is not a member of the list")
+else:
+    print("It is a member of this list")
+b="Hello world"
+if "Hello"in b:
+    print("it is present in b")
+else:
+    print("it is not present in b")
